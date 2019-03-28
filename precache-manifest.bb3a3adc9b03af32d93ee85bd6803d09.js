c5b41b3545459ed4d4088fba9e47c3b8a76348dc
David <daviddrozd2020@gmail.com>
2019-03-28 02:06:21 -0400

    Updates

---
+++ precache-manifest.bb3a3adc9b03af32d93ee85bd6803d09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e36699b53e538de33fc",
+    "url": "/React-MovieDB/static/js/runtime~main.7b815b24.js"
+  },
+  {
+    "revision": "c4e8a1bed98dcdd83c82",
+    "url": "/React-MovieDB/static/js/main.ee794170.chunk.js"
+  },
+  {
+    "revision": "cf769cb09d5731b8abc1",
+    "url": "/React-MovieDB/static/js/2.dd030a29.chunk.js"
+  },
+  {
+    "revision": "c4e8a1bed98dcdd83c82",
+    "url": "/React-MovieDB/static/css/main.c10b8f19.chunk.css"
+  },
+  {
+    "revision": "2ee62fc90a3376b4aae444a2a534ec55",
+    "url": "/React-MovieDB/index.html"
+  }
+];
